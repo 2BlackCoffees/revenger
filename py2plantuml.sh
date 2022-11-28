6d3bbdf6006e3b40b4b83c8f898add873925a2f3
Jean-Philippe Ulpiano <jean-philippe.ulpiano@cloudreach>
2022-11-28 14:54:56 +0100

    Improved output

--- py2plantuml.sh
+++ py2plantuml.sh
@@ -7,6 +7,7 @@ if [[ ! -d $2 ]]; then
     mkdir -p $out_dir
 fi
 $python py2plantuml.py --from_dir $from_dir --out_dir $out_dir && \
-  $plantuml -tsvg $out_dir/*.puml && \
-    $python -m webbrowser $out_dir/full-diagram-detailed.svg
+  echo "Transforming puml to svg" && \
+    $plantuml -tsvg $out_dir/*.puml && \
+      $python -m webbrowser $out_dir/full-diagram-detailed.svg
 
