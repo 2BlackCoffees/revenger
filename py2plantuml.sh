87671b9e7a383d7c07d2ec217f77200302005477
MSarfraz-CR <mohammed.siddiqui@cloudreach.com>
2022-12-11 04:00:25 +0530

    fix review comments

--- py2plantuml.sh
+++ py2plantuml.sh
@@ -2,9 +2,10 @@
 from_dir=$1
 out_dir=$2
 python=python3
-
+plantuml=/opt/homebrew/bin/plantuml
+svg_dep=secure
 function usage(){
-    echo "$(basename $0) [-i | --from_dir ] [ -o | --out_dir] [-d | --dependency_install] [-h | --help]"
+    echo "$(basename $0) [-i | --from_dir ] [ -o | --out_dir] [-d | --dependency_install]  [-p | --plantweb_dep_install] [-h | --help]"
 }
 
 while [[ "$1" != "" ]]; do
@@ -18,10 +19,21 @@ while [[ "$1" != "" ]]; do
         shift;
         ;;
         -d | --dependency_install )
+        echo "Installing plantuml dependency for rendering SVC....."
+        if java -version 2>&1 >/dev/null | grep -E "\S+\s+version" ; then
+          wget -O plantuml.jar https://github.com/plantuml/plantuml/releases/download/v1.2022.13/plantuml.jar
+          echo "Finished installing plantuml dependency for the script!!"
+        else
+          echo "Java is needed for using plantuml dependency...Please install Java first" && exit 1
+        fi
+        ;;
+        -p | --plantweb_dep_install )
         echo "Installing plantweb dependency for rendering SVC....."
+        echo "WARNING: Data will be sent to Planweb server, use only for non-sensitive code!!!!!"
         pip3 install plantweb
+        svg_dep=insecure
         echo "Finished installing plantweb dependency for the script!!"
-        ;;                            
+        ;;   
         -h | --help )
         usage;
         exit;
@@ -30,14 +42,29 @@ while [[ "$1" != "" ]]; do
     shift
 done
 
+# Set plantuml to java binary if it exists in current dir
+if [[ -a plantuml.jar ]]; then
+    plantuml="java -jar plantuml.jar"
+fi
 
-plantuml=/opt/homebrew/bin/plantuml
 if [[ ! -d $out_dir ]]; then
     mkdir -p $out_dir
 fi
+
+
 $python py2plantuml --from_dir $from_dir --out_dir $out_dir $@ && \
-  echo "Transforming puml to svg" && \
+  echo "Transforming puml to svg"
+
+
+if [[ "$svg_dep" == "secure" ]]; then
+    echo "Transforming with plantuml"
+    $plantuml -tsvg $out_dir/*.puml
+
+else
+    echo "!!Transforming with plantweb!!"
     cd $out_dir && \
-    plantweb --engine=plantuml ./*.puml && \
-    echo "Open $out_dir/full-diagram-detailed.svg in your browser..."
-      $python -m webbrowser $out_dir/full-diagram-detailed.svg
+      plantweb --engine=plantuml ./*.puml && \
+        echo "Open $out_dir/full-diagram-detailed.svg in your browser..."
+fi
+
+$python -m webbrowser $out_dir/full-diagram-detailed.svg
