2a019cd5fa6117e14091cc55d2649f952e33578c
Jean-Philippe Ulpiano <jean-philippe.ulpiano@cloudreach>
2022-12-11 11:19:02 +0100

    Improved user experience while waiting for svg generation

--- py2plantuml.sh
+++ py2plantuml.sh
@@ -1,4 +1,20 @@
 #!/bin/sh
+create_svg_files() {
+  plantuml=$1
+  out_dir=$2
+  rm $out_dir/*.svg
+  number_files=$(ls $out_dir/*.puml | wc -l | sed 's:[ \s\t]::g')
+  # Much faster with one call
+  $plantuml -tsvg $out_dir/*.puml &
+  pid_plant_uml=$!
+  while [[ $(ps -edf | grep $pid_plant_uml | grep $(basename $plantuml)) ]]; do
+    sleep 1
+    number_files_processed=$(ls $out_dir/*.svg | wc -l | sed 's:[ \s\t]::g')
+    echo "Processed $number_files_processed/$number_files puml files = $((number_files_processed * 100 / number_files))%"
+  done
+  echo "Done!"
+}
+
 from_dir=$1
 out_dir=$2
 shift
@@ -8,8 +24,9 @@ python=python3
 if [[ ! -d $out_dir ]]; then
     mkdir -p $out_dir
 fi
+echo "Generating puml files"
 $python py2plantuml --from_dir $from_dir --out_dir $out_dir $@ && \
   echo "Transforming puml to svg" && \
-    $plantuml -tsvg $out_dir/*.puml && \
+    create_svg_files $plantuml $out_dir && \
       $python -m webbrowser $out_dir/full-diagram-detailed.svg
 
--- py2plantuml/__main__.py
+++ py2plantuml/__main__.py
@@ -15,8 +15,8 @@ def main(from_dir: str, out_dir: str) -> None:
     
     # create the top-level parser
     parser = argparse.ArgumentParser(prog=program_name)
-    parser.add_argument('--from_dir', type=str, help='Specify where to read the python files from')
-    parser.add_argument('--out_dir', type=str, help='Specify where to store all puml files')
+    parser.add_argument('--from_dir', type=str, help='Specify where to read the python files from', required=True)
+    parser.add_argument('--out_dir', type=str, help='Specify where to store all puml files', required=True)
     parser.add_argument('--skip_uses_relation', action="store_true", help='Do not create use relationship')
     parser.add_argument('--info', action="store_true", help='Set logging to info')
     parser.add_argument('--debug', action="store_true", help='Set logging to debug')
@@ -24,7 +24,11 @@ def main(from_dir: str, out_dir: str) -> None:
     args = parser.parse_args()
     if args.from_dir: from_dir = args.from_dir
     if args.out_dir: out_dir = args.out_dir
+
     logger: Logger = Logger(args.info, args.debug, args.trace)
+    if not from_dir.startswith('/'):
+        logger.log_error(f'Source directory ({from_dir}) is invalid, it requires an absolute path! Exiting!')
+        exit(1)
 
     ApplicationService.read_all_python_files(from_dir, out_dir, logger, PythonLanguage(logger), args.skip_uses_relation)
 
