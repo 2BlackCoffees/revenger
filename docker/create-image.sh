9a366e006c2572140688e5470ba3714a6006c282
Jean-Philippe Ulpiano <jean-philippe.ulpiano@cloudreach>
2023-01-05 09:27:59 +0100

    Create multi platform docker

---
+++ docker/create-image.sh
@@ -0,0 +1,14 @@
+basepath=$( dirname -- "$( readlink -f -- "$0" )" )
+pushd $basepath
+cd DotnetPreAdapter
+rm -rf *
+cd ..
+cp -r $basepath/../dotnet-adapter/DotnetPreAdapter/*.cs DotnetPreAdapter
+docker buildx create --name mybuilder
+docker buildx use mybuilder
+docker buildx inspect mybuilder --bootstrap
+docker buildx build \
+  	-f Dockerfile.csharpadapter \
+    --platform linux/arm64,linux/amd64 \
+   	-t 2blackcoffees/py2plantuml_csharpadapter:latest . --push
+popd
