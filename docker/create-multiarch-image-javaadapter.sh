dc3633ce99a5bcb96eab443d24174195b1b6b39f
Jean-Philippe Ulpiano <jeanphi@AirdeJePhilippe.fritz.box>
2023-04-14 19:12:03 +0200

    Development multi architecture java adapter

--- docker/create-multiarch-image-javaadapter.sh
+++ docker/create-multiarch-image-javaadapter.sh
@@ -2,10 +2,10 @@ basepath=$( dirname -- "$( readlink -f -- "$0" )" )
 pushd $basepath
 rm -rf java-adapter
 cp -r $basepath/../java-adapter/ java-adapter
-docker buildx create --name javabuilder
-docker buildx use javabuilder
-docker buildx inspect javabuilder --bootstrap
-docker buildx build \
+docker-buildx create --name javabuilder
+docker-buildx use javabuilder
+docker-buildx inspect javabuilder --bootstrap
+docker-buildx build \
   	-f Dockerfile.javaadapter \
     --platform linux/arm64,linux/amd64 \
    	-t 2blackcoffees/revenger_javaadapter:latest . --push
