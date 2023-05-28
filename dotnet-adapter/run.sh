be8d3f7d9a22d3898296ef4c98cb5ac01e66c3c1
Jean-Philippe Ulpiano <jean-philippe.ulpiano@cloudreach>
2023-05-28 08:21:56 +0200

    Transform the unix path to a windows one when cygpath is available

--- dotnet-adapter/run.sh
+++ dotnet-adapter/run.sh
@@ -3,5 +3,10 @@ from_dir=$1
 out_dir=$2
 shift
 shift
+cygpath -h > /dev/null 2>&1
+if [[ $? == 0 ]]; then
+  from_dir=$(cygpath -w $from_dir)
+  out_dir=$(cygpath -w $out_dir)
+fi
 dotnet run --from_dir $from_dir --out_dir $out_dir $@
 popd >/dev/null 
--- java-adapter/run.sh
+++ java-adapter/run.sh
@@ -2,4 +2,9 @@ from_dir=$1
 out_dir=$2
 shift
 shift
+cygpath -h > /dev/null 2>&1
+if [[ $? == 0 ]]; then
+  from_dir=$(cygpath -w $from_dir)
+  out_dir=$(cygpath -w $out_dir)
+fi
 mvn install && mvn exec:java -Dexec.args="--from_dir $from_dir --out_dir $out_dir $*"
